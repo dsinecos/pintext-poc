c327233c4cb52f9d5d2087a82a26d184cc39b38b
Divyanshu Singh <dsinecos@gmail.com>
2017-08-03 16:04:57 +0530

    Added code to setup pintext table

--- pintext.js
+++ pintext.js
@@ -20,6 +20,14 @@ app.use(express.static(publicPath));
 //app.use(multer().single('fileName'));
 app.use(require('body-parser').urlencoded({ extended: true }));
 
+var pg = require('pg');
+var connectionString = process.env.DATABASE_URL || "pg://postgres:postgres@localhost:5432/pintext";
+var client = new pg.Client(connectionString);
+client.connect();
+
+client.query("DROP TABLE IF EXISTS pintext CASCADE");
+client.query("CREATE TABLE IF NOT EXISTS pintext(user_id SERIAL PRIMARY KEY, title varchar(256), source varchar(512), textsnippet varchar, hash varchar(1024))");
+
 app.get('/', function (req, res) {
     res.sendFile(__dirname + "/public/pintextHomepage.html");
 });
